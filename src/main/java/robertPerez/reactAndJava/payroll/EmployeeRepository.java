1c508b4f30d2caa9eefd732e3d85d5e8c193cc14
Robert <robgaray9111@yahoo.com>
2020-02-21 13:43:58 -0800

    Built user model and set up dummy data

---
+++ src/main/java/robertPerez/reactAndJava/payroll/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package robertPerez.reactAndJava.payroll;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}

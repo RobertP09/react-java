16a8bd5ea6f9c1570a87be552084239cfec5c01f
Robert <robgaray9111@yahoo.com>
2020-02-21 12:41:14 -0800

    First commit

---
+++ src/main/java/robertPerez/reactAndJava/ReactAndJavaApplication.java
@@ -0,0 +1,13 @@
+package robertPerez.reactAndJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactAndJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/robertPerez/reactAndJava/ReactAndJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package robertPerez.reactAndJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactAndJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
